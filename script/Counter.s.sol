79be07e2b9acf36ea57f7dec1adf0626145a8c11
AkhilSharma90 <akhilsails@gmail.com>
2023-01-20 11:26:55 +0530

    working, tested code

---
+++ script/Counter.s.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import {Counter} from "../src/Counter.sol";
+
+contract CounterScript is Script {
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        new Counter();
+        vm.stopBroadcast();
+    }
+}
